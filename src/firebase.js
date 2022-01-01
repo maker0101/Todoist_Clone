f569adc88a7b5ea9a06db97c00dc90d44916bbd8
Max <88189468+maker0101@users.noreply.github.com>
2022-01-01 23:35:58 +0100

    Setup Firestore configuration and connection

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCU6lgWzoqz5MbGVS5s_hF3QLs-bSZ_vUY',
+	authDomain: 'tudoist-0101.firebaseapp.com',
+	projectId: 'tudoist-0101',
+	storageBucket: 'tudoist-0101.appspot.com',
+	messagingSenderId: '710636975740',
+	appId: '1:710636975740:web:862adb858b4d5bcd627bf9',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { db };
+
+// TODO: from tutorial...to be removed later
+//export { firebaseConfig as firebase };
---
+++ src/firebaseCRUD.js
@@ -0,0 +1,18 @@
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+const storage = {
+	projects: [],
+
+	getProjects: async function () {
+		const querySnapshot = await getDocs(collection(db, 'projects'));
+		querySnapshot.forEach((doc) => {
+			this.projects.push(doc.data());
+		});
+		console.log(this.projects);
+		return this.projects;
+	},
+};
+
+// Call this inside index.js to see documents in projects collection
+// storage.getProjects();
