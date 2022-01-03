90218030ec98f2c43ba99b611fc0d20cefbe8aa0
Max <88189468+maker0101@users.noreply.github.com>
2022-01-03 20:11:33 +0100

    Exchange header logo with menu home search items

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,22 +1,40 @@
-import { SiTodoist } from 'react-icons/si';
-import { VscAdd, VscAccount, VscBell, VscQuestion } from 'react-icons/vsc';
+import {
+	VscMenu,
+	VscHome,
+	VscSearch,
+	VscAdd,
+	VscAccount,
+	VscBell,
+} from 'react-icons/vsc';
+import { IoSearchOutline } from 'react-icons/io5'
 
 export default function Header() {
 	return (
 		<header className="header">
 			<nav className="header__nav">
-				<div className="header__logo">
-					<SiTodoist />
+				<div className="header__navLeft">
+					<ul className="header__navLeftList">
+						<li className="header__item header__itemMenu">
+							<VscMenu />
+						</li>
+						<li className="header__item header__itemHome">
+							<VscHome />
+						</li>
+						<li className="header__item header__itemSearch">
+							<span className='header__searchIcon'><IoSearchOutline /></span>
+							<span className='header__searchText'>Search</span>
+						</li>
+					</ul>
 				</div>
-				<div className="header__settings">
-					<ul className='header__settingsList'>
-						<li className="header__settingsItem header_settingsItemAdd">
+				<div className="header__navRight">
+					<ul className="header__navRightList">
+						<li className="header__item header__itemAdd">
 							<VscAdd />
 						</li>
-						<li className="header__settingsItem header_settingsItemNotifications">
+						<li className="header__item header__itemNotifications">
 							<VscBell />
 						</li>
-						<li className="header__settingsItem header_settingsItemProfile">
+						<li className="header__item header__itemProfile">
 							<VscAccount />
 						</li>
 					</ul>
