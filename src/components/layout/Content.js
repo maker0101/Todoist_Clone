c6f3b4270ecbd7d872228cf9d15d12839ba7cf7b
Max <88189468+maker0101@users.noreply.github.com>
2022-01-06 13:42:34 +0100

    Alternative getDocs approach implemented

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,32 +1,54 @@
 import React from 'react';
-import Checkbox from '../Checkbox';
 import AddTaskForm from '../AddTaskForm';
-import getTasks from '../../hooks/getTasks';
-import { VscTrash, VscEdit } from 'react-icons/vsc';
+import {
+	onSnapshot,
+	collection,
+	query,
+	where,
+	getDocs,
+} from 'firebase/firestore';
+import { db } from '../../firebase';
+import TaskListItem from '../TaskListItem';
 
 export default function Content() {
-	const tasks = getTasks();
+	const [tasks, setTasks] = React.useState([]);
+	const [todos, setTodos] = React.useState([]);
+	console.log(tasks);
+	console.log(todos);
+
+	React.useEffect(() => {
+		getTasks();
+	}, []);
+
+	function getTasks() {
+		const tasksQuery = query(
+			collection(db, 'tasks'),
+			where('userId', '==', 'userid1')
+		);
+		const unsubscribe = onSnapshot(tasksQuery, (querySnapshot) => {
+			const tasksSnapshot = querySnapshot.docs.map((doc) => doc.data());
+			setTasks(tasksSnapshot);
+		});
+		return unsubscribe;
+	}
+
+	React.useEffect(() => {
+		const getTodos = async () => {
+			const data = await getDocs(collection(db, 'tasks'));
+			setTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+
+		getTodos();
+	}, []);
 
 	return (
 		<div className="content">
 			<div className="content__container">
 				<h1 className="content__containerTitle">Task List</h1>
 				<ul className="content__tasksList">
-					{tasks.map((task) => {
-						return (
-							<div className="content__taskContainer" key={`${task.id}`}>
-								<li className="content__task">
-									<Checkbox />
-									<span className="content__taskName">{task.name}</span>
-									<span className="content__taskIcons">
-										<VscTrash />
-										<VscEdit />
-									</span>
-								</li>
-								<hr />
-							</div>
-						);
-					})}
+					{tasks.map((task) => (
+						<TaskListItem name={task.name} id={task.id} key={task.id} />
+					))}
 					<div>
 						<AddTaskForm />
 					</div>
