7fb703de4e7e349539fbc97bbc85e0433fa5ad88
Max <88189468+maker0101@users.noreply.github.com>
2022-01-14 20:17:35 +0100

    Further clean up stylesheet, simplify Sidebar html structure and move oroject dot colors of projects into Firestore

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,18 +12,21 @@ export default function Sidebar(props) {
 		<nav
 			className={`sidebar ${props.sidebarIsHidden ? 'sidebar__hidden' : ''}`}
 		>
-			<div className="sidebar__section sidebar__nav">
+			<div className="sidebar__section">
 				{sidebarNavData.map((item) => (
 					<NavLink
-						to={item.to}
 						key={item.id}
-						className="sidebar__sectionGrid sidebar__item"
+						to={item.to}
+						className="sidebar__item"
 						activeclasscame="selected"
 					>
-						<div className={`sidebar__icon ${item.iconClassName}`} style={{color: item.iconColor}}>
+						<div
+							className={`sidebar__icon ${item.iconClassName}`}
+							style={{ color: item.iconColor }}
+						>
 							{item.icon}
 						</div>
-						<div className="sidebar__text">{item.name}</div>
+						<div>{item.name}</div>
 						<div className="sidebar__info">
 							{countTasksOfNavItems(props.tasks, item)}
 						</div>
@@ -31,34 +34,27 @@ export default function Sidebar(props) {
 				))}
 			</div>
 
-			<div className="sidebar__section sidebar__projects">
-				<div className="sidebar__sectionGrid">
-					<span className="sidebar__icon sidebar__iconChevron">
-						<BsChevronDown />
-					</span>
-					<h2 className="sidebar__sectionTitle">Projects</h2>
+			<div className="sidebar__section">
+				<div className="sidebar__item sidebar__sectionTitle">
+					<BsChevronDown className="sidebar__icon sidebar__iconChevron" />
+					<h2>Projects</h2>
 				</div>
-				<ul className="sidebar__projectsList">
-					{props.projects
-						.filter((project) => project.isInbox === false)
-						.map((project) => (
-							<NavLink
-								to={`/project/${project.id}`}
-								key={project.id}
-								activeclassname="selected"
-							>
-								<li className="sidebar__sectionGrid sidebar__item sidebar__project">
-									<span className="sidebar__icon sidebar__dot">
-										<VscCircleFilled />
-									</span>
-									<span className="sidebar_projectName">{project.name}</span>
-									<span className="sidebar__info">
-										{countTasksOfProject(props.tasks, project.id)}
-									</span>
-								</li>
-							</NavLink>
-						))}
-				</ul>
+				{props.projects
+					.filter((project) => project.isInbox === false)
+					.map((project) => (
+						<NavLink
+							to={`/project/${project.id}`}
+							key={project.id}
+							className="sidebar__item"
+							activeclassname="selected"
+						>
+							<VscCircleFilled className="sidebar__icon" style={{ color: project.iconColor }}/>
+							<div>{project.name}</div>
+							<div className="sidebar__info">
+								{countTasksOfProject(props.tasks, project.id)}
+							</div>
+						</NavLink>
+					))}
 			</div>
 		</nav>
 	);
