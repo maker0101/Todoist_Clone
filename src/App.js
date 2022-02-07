2888375c59f9a5a562d9d8594a6b73734374c60a
Max <88189468+maker0101@users.noreply.github.com>
2022-02-07 08:52:02 +0100

    Move contexts into seperate Contexts.jsx component"

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
-import { SelectedProjectContext } from './contexts/SelectedProjectContext';
-import { ProjectModalContext } from './contexts/ProjectModalContext';
-import { TaskModalContext } from './contexts/TaskModalContext';
-import { ProjectFormContext } from './contexts/ProjectFormContext';
-import { TaskFormContext } from './contexts/TaskFormContext';
-import { SidebarContext } from './contexts/SidebarContext';
-import useMediaQuery from './hooks/useMediaQuery';
-import { defaultProject } from './utilities/default-project';
+import Contexts from './components/Contexts';
 
 const App = () => {
-  const { isWidthLarger720 } = useMediaQuery();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(isWidthLarger720);
-  const [selectedProject, setSelectedProject] = useState('');
-  const [isProjectModalOpen, setIsProjectModalOpen] = useState(false);
-  const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
-  const [projectForm, setProjectForm] = useState({});
-  const [taskForm, setTaskForm] = useState({});
-
-  const clearProjectForm = () => setProjectForm(defaultProject);
-
   return (
     <div className='App'>
-      <SelectedProjectContext.Provider
-        value={{ selectedProject, setSelectedProject }}>
-        <SidebarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen }}>
-          <ProjectModalContext.Provider
-            value={{ isProjectModalOpen, setIsProjectModalOpen }}>
-            <TaskModalContext.Provider
-              value={{ isTaskModalOpen, setIsTaskModalOpen }}>
-              <ProjectFormContext.Provider
-                value={{ projectForm, setProjectForm, clearProjectForm }}>
-                <TaskFormContext.Provider value={{ taskForm, setTaskForm }}>
-                  <Header />
-                  <Main />
-                </TaskFormContext.Provider>
-              </ProjectFormContext.Provider>
-            </TaskModalContext.Provider>
-          </ProjectModalContext.Provider>
-        </SidebarContext.Provider>
-      </SelectedProjectContext.Provider>
+      <Contexts>
+        <Header />
+        <Main />
+      </Contexts>
     </div>
   );
 };
---
+++ src/components/Contexts.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { SelectedProjectContext } from '../contexts/SelectedProjectContext';
+import { ProjectModalContext } from '../contexts/ProjectModalContext';
+import { TaskModalContext } from '../contexts/TaskModalContext';
+import { ProjectFormContext } from '../contexts/ProjectFormContext';
+import { TaskFormContext } from '../contexts/TaskFormContext';
+import { SidebarContext } from '../contexts/SidebarContext';
+import useMediaQuery from '../hooks/useMediaQuery';
+import { defaultProject } from '../utilities/default-project';
+
+const Contexts = ({ children }) => {
+  const { isWidthLarger720 } = useMediaQuery();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(isWidthLarger720);
+  const [selectedProject, setSelectedProject] = useState('');
+  const [isProjectModalOpen, setIsProjectModalOpen] = useState(false);
+  const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
+  const [projectForm, setProjectForm] = useState({});
+  const [taskForm, setTaskForm] = useState({});
+
+  const clearProjectForm = () => setProjectForm(defaultProject);
+
+  return (
+    <>
+      <SelectedProjectContext.Provider
+        value={{ selectedProject, setSelectedProject }}>
+        <SidebarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen }}>
+          <ProjectModalContext.Provider
+            value={{ isProjectModalOpen, setIsProjectModalOpen }}>
+            <TaskModalContext.Provider
+              value={{ isTaskModalOpen, setIsTaskModalOpen }}>
+              <ProjectFormContext.Provider
+                value={{ projectForm, setProjectForm, clearProjectForm }}>
+                <TaskFormContext.Provider value={{ taskForm, setTaskForm }}>
+                  {children}
+                </TaskFormContext.Provider>
+              </ProjectFormContext.Provider>
+            </TaskModalContext.Provider>
+          </ProjectModalContext.Provider>
+        </SidebarContext.Provider>
+      </SelectedProjectContext.Provider>
+    </>
+  );
+};
+
+export default Contexts;
